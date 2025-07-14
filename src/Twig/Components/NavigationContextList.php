6533b6d77a6b12eb26a90cc106a5bc12d933e745
will <fournier.wilf@gmail.com>
2025-07-14 17:42:30 +0000

    playing around

--- src/Twig/Components/NavigationContextList.php
+++ src/Twig/Components/NavigationContextList.php
@@ -4,8 +4,12 @@ namespace App\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
+/**
+ * To list available path on navitation context 
+ * To use , you need to create a navigation context
+ */
 #[AsTwigComponent]
-class ArticleList
+class NavigationContextList
 {
     public string $navigationContext = 'articles';
 }
