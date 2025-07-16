d0d06e8841c3b2b4048a85d41d644a9d95a3d84d
will <fournier.wilf@gmail.com>
2025-07-16 16:15:07 +0000

    update script to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ git pull
 composer2 install --no-dev --optimize-autoloader
 php bin/console asset-map:compile
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+php bin/websiteconsole cache:clear --no-warmup &&  php bin/adminconsole cache:clear --no-warmup
